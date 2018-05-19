52d11c5f898208f1e0e575af204daf30ec9e4e16
Hanbi Hans Choi <personabe1984@gmail.com>
2018-05-19 17:36:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(name){
+  kittens.pop(name);
+}
+
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift(name);
+}
+
+function appendKitten(name){
+  var nKittens = kittens;
+  nKittens.push(name);
+  return nKittens;
+}
+
+function prependKitten(name){
+  var nKittens = kittens;
+  nKittens.unshift(name);
+  return nKittens;
+}
+
